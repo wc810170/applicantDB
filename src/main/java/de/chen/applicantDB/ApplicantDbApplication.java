b874123c863c5ef6fddafe61772a3473521aa7e3
Weihao Chen <weihao.chen@fh-muenster.de>
2021-08-25 09:49:18 +0200

    Added Sprint Boot files

---
+++ src/main/java/de/chen/applicantDB/ApplicantDbApplication.java
@@ -0,0 +1,13 @@
+package de.chen.applicantDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicantDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/chen/applicantDB/ApplicantDbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.chen.applicantDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicantDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
