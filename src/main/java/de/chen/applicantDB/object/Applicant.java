1dc66cda7f0e5c172c577cc4bc111ef65c0ebca4
Weihao Chen <weihao.chen@fh-muenster.de>
2021-08-29 13:32:19 +0200

    Added getter and setter for applicant data

--- src/main/java/de/chen/applicantDB/object/Applicant.java
+++ src/main/java/de/chen/applicantDB/object/Applicant.java
@@ -26,4 +26,77 @@ public class Applicant {
     private long desiredSalary;
 
     private String applicationStatus;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDesiredPosition() {
+        return desiredPosition;
+    }
+
+    public void setDesiredPosition(String desiredPosition) {
+        this.desiredPosition = desiredPosition;
+    }
+
+    public String getJobStatus() {
+        return jobStatus;
+    }
+
+    public void setJobStatus(String jobStatus) {
+        this.jobStatus = jobStatus;
+    }
+
+    public long getDesiredSalary() {
+        return desiredSalary;
+    }
+
+    public void setDesiredSalary(long desiredSalary) {
+        this.desiredSalary = desiredSalary;
+    }
+
+    public String getApplicationStatus() {
+        return applicationStatus;
+    }
+
+    public void setApplicationStatus(String applicationStatus) {
+        this.applicationStatus = applicationStatus;
+    }
+
 }
