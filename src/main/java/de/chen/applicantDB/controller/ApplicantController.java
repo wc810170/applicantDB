5dfdbeee1067116271cf244667923cdda27a9bd5
Weihao Chen <weihao.chen@fh-muenster.de>
2021-08-30 14:08:39 +0200

    Added documentation, reformatting.

--- src/main/java/de/chen/applicantDB/controller/ApplicantController.java
+++ src/main/java/de/chen/applicantDB/controller/ApplicantController.java
@@ -3,10 +3,12 @@ package de.chen.applicantDB.controller;
 import de.chen.applicantDB.entity.Applicant;
 import de.chen.applicantDB.repo.ApplicantRepo;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
+/**
+ * This method contains functions to show, create and modify applicant data.
+ * @Author Weihao Chen
+ */
 @RestController
 @RequestMapping("/Applicant")
 public class ApplicantController {
@@ -19,12 +21,28 @@ public class ApplicantController {
 
     }
 
+    /**
+     * Lists all data in the database.
+     * @return Every entry in the database in json format.
+     */
     @GetMapping(path = "/all")
     public List<Applicant> getAll() {
 
         return applicantRepo.findAll();
     }
 
+    /**
+     * Creates a new applicant entry in the database.
+     * @param firstName Vorname
+     * @param lastName  Nachname
+     * @param email E-Mail-Adresse
+     * @param phoneNumber Telefonnummer
+     * @param desiredPosition gewünschte Position
+     * @param jobStatus Beschäftigungsart (Vollzeit/Teilzeit/Azubi/Werkstudent)
+     * @param desiredSalary Gehaltswunsch (pro Jahr, €)
+     * @param applicationStatus Status im Bewerbungsprozess (Neu/Erstgespräch/Zweitgespräch/Angebot-Gemacht/Eingestellt)
+     * @return A newly created applicant, saved in the database.
+     */
     @PostMapping(path = "/add")
     public @ResponseBody Applicant addNewApplicant(@RequestParam String firstName, @RequestParam String lastName,
                                                 @RequestParam String email, @RequestParam long phoneNumber,
@@ -44,6 +62,11 @@ public class ApplicantController {
 
     }
 
+    /**
+     * Updates existing applicant's information, by finding them in the first place with findById()
+     * @param applicant
+     * @return Existing applicant with newly edited information.
+     */
     @PostMapping(path = "/update")
     public @ResponseBody Applicant updateApplicant(Applicant applicant) {
 
--- src/main/java/de/chen/applicantDB/entity/Applicant.java
+++ src/main/java/de/chen/applicantDB/entity/Applicant.java
@@ -1,11 +1,14 @@
 package de.chen.applicantDB.entity;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Applicant {
@@ -13,39 +16,53 @@ public class Applicant {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotEmpty
     @NotBlank
+    @NotNull
     private String firstName;
 
+    @NotEmpty
     @NotBlank
+    @NotNull
     private String lastName;
 
+    @NotEmpty
     @NotBlank
+    @NotNull
     @Email
     private String email;
 
+    @NotEmpty
     @NotBlank
+    @NotNull
     private long phoneNumber;
 
+    @NotEmpty
     @NotBlank
+    @NotNull
     private String desiredPosition;
 
+    @NotEmpty
     @NotBlank
+    @NotNull
     private String jobStatus;
 
+    @NotEmpty
     @NotBlank
+    @NotNull
     private long desiredSalary;
 
+    @NotEmpty
     @NotBlank
+    @NotNull
     private String applicationStatus;
 
     public long getId() { return id; }
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
@@ -53,6 +70,7 @@ public class Applicant {
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -60,6 +78,7 @@ public class Applicant {
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -67,6 +86,7 @@ public class Applicant {
     public long getPhoneNumber() {
         return phoneNumber;
     }
+
     public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
@@ -74,6 +94,7 @@ public class Applicant {
     public String getDesiredPosition() {
         return desiredPosition;
     }
+
     public void setDesiredPosition(String desiredPosition) {
         this.desiredPosition = desiredPosition;
     }
@@ -81,6 +102,7 @@ public class Applicant {
     public String getJobStatus() {
         return jobStatus;
     }
+
     public void setJobStatus(String jobStatus) {
         this.jobStatus = jobStatus;
     }
@@ -88,6 +110,7 @@ public class Applicant {
     public long getDesiredSalary() {
         return desiredSalary;
     }
+
     public void setDesiredSalary(long desiredSalary) {
         this.desiredSalary = desiredSalary;
     }
@@ -95,6 +118,7 @@ public class Applicant {
     public String getApplicationStatus() {
         return applicationStatus;
     }
+
     public void setApplicationStatus(String applicationStatus) {
         this.applicationStatus = applicationStatus;
     }
